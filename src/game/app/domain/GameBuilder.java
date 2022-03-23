c2e2f66174389f3f95b68a0bd14b7b294c0d7574
Rybchits <Grinya110@gmail.com>
2022-03-23 22:08:10 +0300

    Added builder config and first dictionary. Installed json.simple package

--- src/game/app/domain/GameBuilder.java
+++ src/game/app/domain/GameBuilder.java
@@ -4,14 +4,61 @@ import game.app.domain.exceptions.InvalidFieldSideException;
 import game.app.domain.exceptions.InvalidNameDictionaryException;
 import game.app.domain.gamefield.GameField;
 import org.jetbrains.annotations.NotNull;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 
 public class GameBuilder {
+
+    private static final String pathConfig = ".\\src\\resources\\builder_config.json";
+
+    private static final String keyAvailableFieldSizes = "availableFieldSize";
+    private static final String keyDefaultFieldSide = "defaultFieldSide";
+    private static final String keyAvailableDictionaries = "availableDictionaries";
+
     // Единственный экземпляр класса
     private static GameBuilder instance;
-    private GameBuilder(){}
+
+    private GameBuilder() {
+
+        // Чтение конфигураций из файла
+        try(FileReader reader = new FileReader(pathConfig)) {
+
+            JSONParser parser = new JSONParser();
+            JSONObject rootJsonObject = (JSONObject) parser.parse(reader);
+
+            // Получить конфигурации
+            long defaultFieldSideValue = (long)rootJsonObject.get(keyDefaultFieldSide);
+            JSONArray availableFieldSizesJson = (JSONArray)rootJsonObject.get(keyAvailableFieldSizes);
+            JSONObject availableDictionariesJson = (JSONObject)rootJsonObject.get(keyAvailableDictionaries);
+
+            // Устанавливаем список допустимых словарей
+            for (String name : (Iterable<String>) availableDictionariesJson.keySet()) {
+                _availableDictionaries.put(name, (String) availableDictionariesJson.get(name));
+                if (nameSelectedDictionary == null) nameSelectedDictionary = name;
+            }
+
+            // Устанавливаем список допустимых значений размеров поля
+            availableFieldSize.clear();
+            for (Object el : availableFieldSizesJson){
+                var value = (long)el;
+                availableFieldSize.add((int)value);
+            }
+
+            // Устанавливаем размер поля по умолчанию
+            _fieldSide = (int)defaultFieldSideValue;
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static GameBuilder getInstance(){
         if(instance == null){ instance = new GameBuilder(); }
@@ -48,7 +95,7 @@ public class GameBuilder {
 
 
     // Словари
-    private HashMap<String, String> _availableDictionaries;
+    private final HashMap<String, String> _availableDictionaries = new HashMap<>();
     private String nameSelectedDictionary;
 
     public void setDictionaryByName(String name) {
@@ -60,8 +107,23 @@ public class GameBuilder {
 
     public Set<String> getNamesDictionaries() { return _availableDictionaries.keySet(); }
 
-    // Todo сделать загрузку слов из выбранного словаря
-    public Set<String> loadWordsFromSelectedDictionaries() { return new HashSet<>(); }
+    public Set<String> loadWordsFromSelectedDictionaries() {
+        if (nameSelectedDictionary == null)
+            throw new InvalidNameDictionaryException();
+
+        HashSet<String> words = new HashSet<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(_availableDictionaries.get(nameSelectedDictionary)))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                words.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return words;
+    }
 
 
     // Порождающие методы
